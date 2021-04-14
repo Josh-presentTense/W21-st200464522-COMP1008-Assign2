1bb5214066eea20f7e072d422ba1f58ed152c138
Josh-presentTense <200464522@student.georgianc.on.ca>
2021-04-14 19:58:28 -0400

    Created first view of the program. Added MenuView fxml file and MenuView controller.

---
+++ src/controllers/CollectionManagerMenuViewController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CollectionManagerMenuViewController implements Initializable {
+
+    @FXML
+    private ComboBox<String> categoryComboBox;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
